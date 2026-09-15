5bba8b70cfcf84e2d9d75d053e7bfe71bb0bd83a
khaled057 <khaledmhosny@proton.me>
2026-09-15 10:10:49 +0300

    undo the mistake of adding node_modules

--- src/routes/userroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createUser } = require("../controllers/usercontroller");
-const { protect,isAdmin} =require("../middleware/authmiddleware");
-
-const router = express.Router();
-
-router.post("/", createUser);
-
-module.exports = router;
